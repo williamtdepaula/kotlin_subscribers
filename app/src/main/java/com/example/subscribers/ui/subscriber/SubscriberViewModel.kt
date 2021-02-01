e24ac9d961765d9206a735529e752b893097dd37
William Tristão de Paula <william@grupowi.com.br>
2021-02-01 19:41:21 -0300

    ::First commit::

---
+++ app/src/main/java/com/example/subscribers/ui/subscriber/SubscriberViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.subscribers.ui.subscriber
+
+import androidx.lifecycle.ViewModel;
+
+class SubscriberViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/subscribers/ui/subscriber/subscriberFragment.kt
@@ -0,0 +1,23 @@
+package com.example.subscribers.ui.subscriber
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.subscribers.R
+
+class subscriberFragment : Fragment() {
+
+    private lateinit var viewModel: SubscriberViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.subscriber_fragment, container, false)
+    }
+
+
+}
